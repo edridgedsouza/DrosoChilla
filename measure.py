697473bae0758bcbfe65c98a2afad509886e94e6
Edridge D'Souza <edsouza@umass.edu>
2016-10-21 15:51:10 -0400

    Increased sleep delay during measurements

--- measure.py
+++ measure.py
@@ -12,10 +12,13 @@ def initializeIO(pin):
 def measure(s):
 	#pi = pigpio.pi()
 	#sensor = DHT22.sensor(pi, 4) # I believe this number depends on what GPIO pin you use
-	# Time in mdY, HMS format. Sorry, non-Americans
 	t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') # Need Plotly-compatible format
+	
 	s.trigger()
-	time.sleep(0.1) # Need to let it sleep so the trigger can finish before printing
+	time.sleep(1) 
+	# Need to let it sleep so the trigger can finish before printing.
+	# This doesn't cause an issue because the measurement is already taken at this point.
+
 	return t, round(s.temperature(),3), round(s.humidity(),3)
 
 

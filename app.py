fef7c6ebe076b930f41cff846e0f5c38246e5d90
Edridge D'Souza <edsouza@umass.edu>
2016-10-21 13:25:35 -0400

    Change app.py number of entries selected

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/log')
 def makelog():
 	header = 'time\ttemp\thum\n'
-	LINES = 500
+	LINES = 2880 # Theoretical number of measurements in 24 hours
 	with open('./templog.txt','r') as file:
 		data = file.read()
 	return header + data

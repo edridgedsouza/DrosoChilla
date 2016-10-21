34801300e06a7724e75ba0754e5856b7257e94db
Edridge D'Souza <edsouza@umass.edu>
2016-10-21 15:46:24 -0400

    Fixed datetime format issues

--- measure.py
+++ measure.py
@@ -13,7 +13,7 @@ def measure(s):
 	#pi = pigpio.pi()
 	#sensor = DHT22.sensor(pi, 4) # I believe this number depends on what GPIO pin you use
 	# Time in mdY, HMS format. Sorry, non-Americans
-	t = datetime.datetime.now().strftime('%a %m/%d/%Y %H:%M:%S')
+	t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') # Need Plotly-compatible format
 	s.trigger()
 	time.sleep(0.1) # Need to let it sleep so the trigger can finish before printing
 	return t, round(s.temperature(),3), round(s.humidity(),3)

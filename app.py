e171aacea35f3b77ae1545e5abe9fd59298bc33d
Edridge D'Souza <edsouza@umass.edu>
2017-02-07 15:00:12 -0500

    Fixed bugs so log/errorlog API work correctly now

--- app.py
+++ app.py
@@ -13,45 +13,48 @@ todayString = datetime.strftime(today, "%Y-%m-%d")
 yesterday = datetime(today.year, today.month, today.day-1)
 yesterdayString = datetime.strftime(yesterday, "%Y-%m-%d")
 
-def filterDates(rawlines, start = todayString, end = yesterdayString):
-	endDate = datetime.strptime(start, "%Y-%m-%d")
-	startDate = datetime.strptime(end, "%Y-%m-%d")
+def filterDates(rawlines, start, end):
+	startDate = datetime.strptime(start, "%Y-%m-%d")
+	endDate = datetime.strptime(end, "%Y-%m-%d")
 	endDate = datetime(endDate.year, endDate.month, endDate.day + 1) # Because you want until 23:59 on the end date
-	
+
 	dates = [i.split('\t')[0] for i in rawlines]
-	parsedDates = parsedDates = [datetime.strptime(i, '%Y-%m-%d %H:%M:%S') for i in dates]
+	parsedDates = [datetime.strptime(i, '%Y-%m-%d %H:%M:%S') for i in dates]
 
 	truth = [startDate <= day <= endDate for day in parsedDates]
-
 	matchingLines = [rawlines[i] for i in range(len(truth)) if truth[i]]
 	return matchingLines
 
 
-@app.route('/log')
+@app.route('/log', methods=['GET'])
 def makelog():
+	startParam = request.args.get('start', yesterdayString)
+	endParam = request.args.get('end', todayString)
+
 	with open('./datalog.txt','r') as file:
 		data = file.readlines()
+	filteredLines = filterDates(data, startParam, endParam)
 	# Ignore error outputs to log, just in case
 	def isTrueLine(line):
 		pattern = re.compile('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\t(?!-999)[\d\.]{0,5}(?!-999)\t[\d\.]{0,5}')
 		return bool(pattern.search(line))
-	truelines = [line for line in data if isTrueLine(line)]
+	truelines = [line for line in filteredLines if isTrueLine(line)] # Is this even necessary?
 	finaldata = ''.join(truelines)
 	header = 'time\ttemp\thum\n'
 	return header + finaldata
 
 
-@app.route('/errlog')
+@app.route('/errlog', methods=['GET'])
 def makeErrlog():
-	# Placeholder for smart date selection
-	# When finished doing for regular log, copy over here.
+	startParam = request.args.get('start', yesterdayString)
+	endParam = request.args.get('end', todayString)
 
-
-	header = 'time\ttemp\thum\n'
 	with open('./errorlog.txt') as file:
 		data = file.readlines()
-	lines = [line for line in data]
-	finaldata = ''.join(lines)
+	filteredLines = filterDates(data, startParam, endParam)
+	truelines = [line for line in filteredLines]
+	finaldata = ''.join(truelines)
+	header = 'time\ttemp\thum\n'
 	return header + finaldata
 
 
